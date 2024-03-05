dbf67e85ee10e0352c7cec173d8cc08cf57fc18e
Jovelasirot <jovelasirot@gmail.com>
2024-03-05 14:33:28 +0100

    Es 4

--- src/es3/Es3.java
+++ src/es3/Es3.java
@@ -1,8 +1,20 @@
 package es3;
 
+import java.util.Scanner;
+
 public class Es3 {
     public static void main (String[] args){
-        System.out.println("Es3")
+        System.out.println("Es3");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a phrase");
+
+
+        scanner.close();
         ;}
+
+    public static String commasAdder(String phrase){
+        
+    }
 }
--- src/es4/Es4.java
+++ src/es4/Es4.java
@@ -1,8 +1,23 @@
 package es4;
 
+import java.util.Scanner;
+
 public class Es4 {
     public static void main (String[] args){
-        System.out.println("Es4")
+        System.out.println("Es4");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Write a number");
+int x = scanner.nextInt();
+        System.out.println("CountDown from " + x);
+     countDown(x);
+
+        scanner.close();
         ;}
+
+    public static void countDown(int num){
+        for (int i = num; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
 }
