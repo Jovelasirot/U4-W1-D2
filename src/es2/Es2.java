7cb15066ac4de252918c524cf60c4be15b27c369
Jovelasirot <jovelasirot@gmail.com>
2024-03-05 14:15:55 +0100

    Es 2

--- src/es2/Es2.java
+++ src/es2/Es2.java
@@ -1,8 +1,29 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Es2 {
     public static void main (String[] args){
-        System.out.println("Es2")
+        System.out.println("Es2");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a number");
+        int number = scanner.nextInt();
+        System.out.println(number);
+        System.out.println(numberToWord(number));
+
+        scanner.close();
         ;}
+
+    public static String numberToWord(int x) {
+        return switch (x) {
+            case 0 -> "zero";
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            default -> "Error";
+        };
+
+    }
 }
